2a38718adb5d1ea07198d53382ddce352e5c7367
juno-choi <ililil9482@naver.com>
2023-08-25 09:14:22 +0900

    [JDBC 개발] 수정, 삭제

--- src/test/java/com/example/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/com/example/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,11 +1,11 @@
 package com.example.jdbc.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import com.example.jdbc.domain.Member;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -22,7 +22,17 @@ class MemberRepositoryV0Test {
         //findById
         Member findMember = repository.findById(member.getMemberId());
         log.info("findMember = {}", findMember);
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+            .isInstanceOf(NoSuchElementException.class);
     }
 
 }
