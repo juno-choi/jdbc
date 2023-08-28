846c0d172d238609b965725ed7f16bbd14e8a1ab
juno-choi <ililil9482@naver.com>
2023-08-28 12:47:04 +0900

    [DataSource ConnectionPool] Hikari Pool 사용에 대한 이해

--- src/test/java/com/example/jdbc/connection/ConnectionTest.java
+++ src/test/java/com/example/jdbc/connection/ConnectionTest.java
@@ -4,6 +4,7 @@ import static com.example.jdbc.connection.ConnectionConst.PASSWORD;
 import static com.example.jdbc.connection.ConnectionConst.URL;
 import static com.example.jdbc.connection.ConnectionConst.USERNAME;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -25,14 +26,30 @@ public class ConnectionTest {
     }
 
     @Test
-    void dataSourceDriverManager(){
+    void dataSourceDriverManager() throws SQLException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME,
             PASSWORD);
-
+        useDataSource(dataSource);
     }
 
-    private void userDataSource(DataSource dataSource) throws SQLException {
+    private void useDataSource(DataSource dataSource) throws SQLException {
         Connection connection1 = dataSource.getConnection();
+        Connection connection2 = dataSource.getConnection();
+        log.info("connection={}, class={}", connection1, connection1.getClass());
+        log.info("connection={}, class={}", connection2, connection2.getClass());
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
 }
